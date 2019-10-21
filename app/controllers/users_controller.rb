b6d0f35b56187c1a7d3a28eb17cf4742617fa57d
Adil Naimi <adilnm23@hotmail.com>
2019-10-20 20:20:32 -0500

    add a destroy method to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    reset_session
+    redirect_to login_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :age, :email, :phone, :password)
