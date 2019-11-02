9ae5e0d3635ef0705c7d57b3c9d5c4ec026d9cad
Adil Naimi <adilnm23@hotmail.com>
2019-11-01 22:57:20 -0500

    Add validations to the signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,18 @@ class UsersController < ApplicationController
 
   def create
     @user=User.new(user_params)
-    if @user.save
+
+    if @user.valid?
+      @user.save
       session[:user_id]=@user.id
       redirect_to root_path
     else
+      errors=@user.errors.messages.map do |k,v|
+        v.map do |s|
+          "#{k} #{s}"
+        end
+      end
+      @errors=errors.flatten
       render 'users/new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :name, :age, :email,:phone, :password, presence: true
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
+  validates :email, format: { with: /.*@.*/}
   has_secure_password
   has_many :reviews
   has_many :restaurants, through: :reviews
